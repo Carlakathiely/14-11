1612a354e4cc82b65c9e5d1cff571c1053da6a9d
kathy <carlakathiely@gmail.com>
2024-11-19 11:24:08 -0300

    Carla 15

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
 nome = input("insira seu nome")
-#aqui o sistema pede o nome do usuario
-idade = 15(input("insira sua idade"))
-#aqui mostra o nome e idade do usuario
-print("bem vindo {nome}, voce tem {idade} anos"
-      
-print(`como posso te ajudar hoje´´)
-   def menu():   
- print("1-bebidas")
- print("2-salgados")
- print("3-doces")
- opc_user=int(input("digite o número da opçao desejada")
-        
-if opc_user==1:
-  print("temos coca,franta e guarana")
 
-  elif opc_user==2:
-  print("temos coxinha,risole e impadao")
 
-  elif opc_user==3:
-  print(temos brigadeiros)
 
-  else:
-  print("opcao incorreta! digite um numero de 1 a 3")
+idade =int(input("digite sua idade"))
+
+
+
+print(f"bem vindo {nome},voce tem {idade} anos!")
+
+
+
+print("como posso te ajudar hoje")
+
+
+
+def menu():
+
+print("1- bebidas")
+
+print("2- salgados")
+
+print("3- doces")
+
+
+
+opc_user = int(input("digite o numero da opção desejada"))
+
+
+
+if opc_user == 1:
+
+    print("temos coca, fanta e guarana")
+
+
+
+elif opc_user == 2:
+
+    print("temos brigadeiro")
+
+
+
+else:
+
+    print("opção incorreta! digite um numero de 1 a 3") 
+
+
+
+menu()
